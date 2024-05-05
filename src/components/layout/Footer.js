e1f52d733bbf321b0a147b39074611ebb43e2458
Artur Mineiro <105992706+ArturMineiro@users.noreply.github.com>
2024-05-05 10:00:43 -0300

    react icons
    react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,18 @@
+
+//import { IconName } from "react-icons/ai";
+import {FaBeer, FaInstagram } from 'react-icons/fa'
+import {FaFacebook} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer(){
-return<footer>Rodape</footer>
+return<footer>
+    <ul className={styles.social_list}> 
+        <li>    <FaBeer /> </li>
+        <li>    <FaFacebook /> </li>
+        <li>    <FaInstagram/> </li>
+
+    </ul>
+    </footer>
 }
 
 export default Footer
